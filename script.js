d49201dbd5d007f07bd3e53d865de4aac0d498b3
sharmagaurav1912 <guri07cr@gmail.com>
2024-03-01 21:16:13 +0530

    worked on product page

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let count = 0;
 
 let cartCount = document.getElementById('cart-count');
 cartCount.innerText = count;
-// cartCount.classList.add('display');
 
 let cartButton = document.querySelectorAll('#cart-btn');
 cartButton.forEach((e) => {
@@ -11,36 +10,25 @@ cartButton.forEach((e) => {
 
         if (isUserLogged) {
             console.log(n.target);
-            // cartCount.style.display = "";
             count ++;
             cartCount.innerText = count;
         } else {
             console.log("login First");
-            // alert('login First');
             window.location.href = "#login"
         }
         
     })
 })
 
-let proName = document.querySelectorAll('#pro-name');
-let proDes = document.querySelectorAll('#pro-des');
-let proPrice = document.querySelectorAll('#pro-price');
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 let buyButton = document.querySelectorAll('#buy-btn');
 buyButton.forEach((e) => {
     e.addEventListener("click",(n) => {
+
         if (isUserLogged) {
             console.log(n.target.parentNode);
-
-            window.open('http://127.0.0.1:5500/proPage.html');
-
-            document.querySelector('selected-pro-price')
-            .innerHTML = e.proPrice;
-            document.querySelector('selected-pro-name')
-            .innerHTML = e.proName;
-            document.querySelectorAll('selected-pro-des')
-            .innerHTML = e.proDes;
+            // window.open('http://127.0.0.1:5500/proPage.html');
 
         } else {
             console.log("login First");
@@ -50,6 +38,38 @@ buyButton.forEach((e) => {
         
     })
 })
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let proImg = document.querySelectorAll('#pro-img');
+proImg.forEach((e) => {
+    e.addEventListener("click",(n) => {
+
+        var img = n.target.parentNode.childNodes[1].src;
+        var name = n.target.parentNode.childNodes[3].innerText;
+        var description = n.target.parentNode.childNodes[5].innerText;
+        var price = n.target.parentNode.childNodes[7].innerText;
+        
+            console.log(img);
+            console.log(name);
+            console.log(description);
+            console.log(price);
+
+            // window.open('http://127.0.0.1:5500/proPage.html');
+
+            // document.querySelector('#selected-pro-img').src = img;
+            // document.querySelector('#selected-pro-price').innerText = price;
+            // document.querySelector('#selected-pro-name').innerText = name;
+            // document.querySelector('#selected-pro-des').innerText = description;
+
+            // console.log(document.querySelector('#selected-pro-price').innerText);
+            // console.log(document.querySelector('#selected-pro-name').innerText);
+            // console.log(document.querySelector('#selected-pro-des').innerText);
+        
+    })
+})
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 let login = document.getElementById('login');
 login.addEventListener("click",() => {
@@ -69,3 +89,24 @@ login.addEventListener("click",() => {
     }
     
 })
+
+//             document.querySelector('#selected-pro-img')
+//             .addEventListener("click",()=>{
+//                 console.log("image clicked");
+//             })
+//             document.querySelector('#selected-pro-price')
+//             .addEventListener("click",()=>{
+//                 console.log("price clicked");
+//             })
+//             document.querySelector('#selected-pro-name')
+//             .addEventListener("click",()=>{
+//                 console.log("name clicked");
+//             })
+//             document.querySelector('#selected-pro-des')
+//             .addEventListener("click",()=>{
+//                 console.log("description clicked");
+//             })
+
+// function click(params) {
+//     console.log("button clicked");
+// }
