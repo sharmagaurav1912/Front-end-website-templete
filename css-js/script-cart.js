fa869e98a335af5d12527c418b0aa8eae5508ad1
sharmagaurav1912 <guri07cr@gmail.com>
2024-03-09 17:40:45 +0530

    worked on navigation and redirection between different pages

--- css-js/script-cart.js
+++ css-js/script-cart.js
@@ -6,7 +6,10 @@ let tax = 5;
 let grandTotal = 0;
 
 document.addEventListener('DOMContentLoaded',
-async function getData() {
+async function getData(e) {
+
+    e.preventDefault;
+
     const response = await fetch('https://dummyjson.com/products');
     const data = await response.json();
 
@@ -15,49 +18,45 @@ async function getData() {
 
             let proCont = document.getElementById("cart-pro-container");
             let product = document.createElement('div');
-            product.classList.add('row')
-            product.classList.add('bg-light')
+            product.classList.add('row');
+            product.classList.add('bg-light');
 
             product.id = "cart-product";
             product.innerHTML = `
-
-            <div class="col-lg-1 col-sm-4 border">
+            <div class="col-lg-1 col-sm-4 cart-div">
                 <img src="${pro.thumbnail}" alt="" id="cart-pro-img">
             </div>
-            <div class="col-lg-5 col-sm-8 py-2">
+            <div class="col-lg-5 col-sm-8 py-2 cart-div">
                 <span id="cart-product-name">${pro.title}</span>
                 <br>
                 <span id="cart-product-des">${pro.description}</span>
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 py-2">
+            <div class="col-lg-2 col-md-4 col-sm-4 py-2 cart-div">
+                <span>Price</span>
+                <br>
                 <span id="cart-product-price">₹${pro.price}/-</span>
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 py-2">
+            <div class="col-lg-2 col-md-4 col-sm-4 py-2 cart-div">
+                <span>Quantity</span>
+                <br>
                 <input type="number" id="cart-pro-quantity" value="1" class="w-100">
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 py-2">
-                <span id="cart-pro-total">Total</span>
+            <div class="col-lg-2 col-md-4 col-sm-4 py-2 cart-div">
+                <span>Total</span>
+                <br>
+                <span id="cart-pro-total">₹${pro.price}/-</span>
             </div>
-            
             `;
-
-            
             proCont.appendChild(product);
 
-            // console.log(pro.price);
-            
-            let proQuantity = document.getElementById('cart-pro-quantity');
-            let proTotal = document.getElementById('cart-pro-total');
-    
-            proTotal.innerText = `₹ ${pro.price} /-`;
-            // proTotal.innerText = `₹${proQuantity.value * pro.price}/-`;
+            totalAmount += pro.price;
+            payableTax = totalAmount/100*tax;
+            grandTotal = totalAmount + payableTax;
 
             document.getElementById('total-amount')
             .innerText = `Total Amont - ₹${totalAmount}/-`;
-
             document.getElementById('amount-tax')
-            .innerText = `Payable Tax - ${tax}%`;
-
+            .innerText = `GSTIN - ${tax}%`;
             document.getElementById('grand-total')
             .innerText = `Grand Total - ₹${grandTotal}/-`
         }  
