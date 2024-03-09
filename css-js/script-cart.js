54aaa098711337685bc62bb534745cb1411c6b50
sharmagaurav1912 <guri07cr@gmail.com>
2024-03-09 12:32:44 +0530

    worked on total amount and grand total after applying certain tax

--- css-js/script-cart.js
+++ css-js/script-cart.js
@@ -17,24 +17,25 @@ async function getData() {
             let product = document.createElement('div');
             product.classList.add('row')
             product.classList.add('bg-light')
+
             product.id = "cart-product";
             product.innerHTML = `
 
             <div class="col-lg-1 col-sm-4 border">
                 <img src="${pro.thumbnail}" alt="" id="cart-pro-img">
             </div>
-            <div class="col-lg-5 col-sm-8 py-2 border">
+            <div class="col-lg-5 col-sm-8 py-2">
                 <span id="cart-product-name">${pro.title}</span>
                 <br>
                 <span id="cart-product-des">${pro.description}</span>
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 py-2 border">
+            <div class="col-lg-2 col-md-4 col-sm-4 py-2">
                 <span id="cart-product-price">₹${pro.price}/-</span>
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 py-2 border">
+            <div class="col-lg-2 col-md-4 col-sm-4 py-2">
                 <input type="number" id="cart-pro-quantity" value="1" class="w-100">
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 py-2 border">
+            <div class="col-lg-2 col-md-4 col-sm-4 py-2">
                 <span id="cart-product-total">Total</span>
             </div>
             
@@ -80,20 +81,23 @@ themeImg.addEventListener("click",()=>{
         themeImg.src="img/sun.png";
         lightTheme = false;
 
-        document.getElementById('cart-nav')
-        .classList.add('bg-dark')
-        document.getElementById('cart-nav')
-        .classList.add('text-white')
+        // document.getElementById('cart-nav')
+        // .classList.add('bg-dark');
+        // document.getElementById('cart-nav')
+        // .classList.add('text-white');
 
         document.getElementById('cart-product')
-        .classList.add('bg-dark')
+        .classList.remove('bg-light');
+
         document.getElementById('cart-product')
-        .classList.add('text-white')
+        .classList.add('bg-dark');
+        document.getElementById('cart-product')
+        .classList.add('text-white');
 
         document.getElementById('cart-total')
-        .classList.add('bg-dark')
+        .classList.add('bg-dark');
         document.getElementById('cart-total')
-        .classList.add('text-white')
+        .classList.add('text-white');
 
         // document.querySelector('#cart-nav','#cart-product','#cart-total')
         // .classList.add('bg-dark');
@@ -107,20 +111,23 @@ themeImg.addEventListener("click",()=>{
         themeImg.src="img/crescent-moon.png";
         lightTheme = true; 
 
-        document.getElementById('cart-nav')
-        .classList.remove('bg-dark')
-        document.getElementById('cart-nav')
-        .classList.remove('text-white')
+        // document.getElementById('cart-nav')
+        // .classList.remove('bg-dark');
+        // document.getElementById('cart-nav')
+        // .classList.remove('text-white');
 
         document.getElementById('cart-product')
-        .classList.remove('bg-dark')
+        .classList.add('bg-light');
+
         document.getElementById('cart-product')
-        .classList.remove('text-white')
+        .classList.remove('bg-dark');
+        document.getElementById('cart-product')
+        .classList.remove('text-white');
 
         document.getElementById('cart-total')
-        .classList.remove('bg-dark')
+        .classList.remove('bg-dark');
         document.getElementById('cart-total')
-        .classList.remove('text-white')
+        .classList.remove('text-white');
 
         // document.querySelector('#cart-nav','#cart-product','#cart-total')
         // .classList.remove('bg-dark');
